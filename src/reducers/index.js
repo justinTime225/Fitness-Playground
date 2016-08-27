415813cebefb89a072f71e3731528cb2f048d46d
Justin <justinminh225@gmail.com>
2016-08-27 12:19:57 -0700

    Record customWorkout and redux form on application state

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,11 @@
 import { combineReducers } from 'redux';
 import basicWorkout from './basicWorkouts.reducer.js';
+import customWorkout from './customWorkouts.reducer.js';
+import { reducer as formReducer } from 'redux-form';
 const rootReducer = combineReducers({
-  basicWorkout: basicWorkout
+  basicWorkout: basicWorkout,
+  customWorkout: customWorkout,
+  form: formReducer
 });
 
 export default rootReducer;
