ab54b769c9c051b44ce86a2a269eeb4a48c33ba4
Justin <justinminh225@gmail.com>
2016-08-26 19:21:30 -0700

    Unit testing with supertest GET for chest and back

---
+++ test/test-server.js
@@ -0,0 +1,36 @@
+const should = require('chai').should();
+const expect = require('chai').expect;
+const request = require('supertest');
+const app = require('../server');
+
+
+describe('Fitness Data', function() {
+  it('should resond with chest data', (done) => {
+    request(app)
+      .get('/chest')
+      .expect(200)
+      .expect((res, err) => {
+        res.body.length.should.equal(5);
+        res.body[0].name.should.equal('InclinePress');
+        res.body[1].name.should.equal('FlatPress');
+        res.body[2].name.should.equal('DeclinePress');
+        res.body[3].name.should.equal('ChestFlyes');
+        res.body[4].name.should.equal('Dips');
+      })
+      .end(done);
+  });
+  it('should resond with back data', (done) => {
+    request(app)
+      .get('/back')
+      .expect(200)
+      .expect((res, err) => {
+        res.body.length.should.equal(5);
+        res.body[0].name.should.equal('RomanianDeadlift');
+        res.body[1].name.should.equal('PullUp');
+        res.body[2].name.should.equal('LateralPulldown');
+        res.body[3].name.should.equal('DumbbellRow');
+        res.body[4].name.should.equal('SeatedRow');
+      })
+      .end(done);
+  });
+});
