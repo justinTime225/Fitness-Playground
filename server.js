8167198ed80bfa396557dbe72f0946ad30592d6c
Justin <justinminh225@gmail.com>
2016-08-26 19:20:31 -0700

    Add body parsers and resolve port error with mocha

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const path = require('path');
 const port = process.env.PORT || 8080;
+const bodyParser = require('body-parser');
 var app = express();
+module.exports = app;
 
 app.use(express.static(__dirname));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 require('./db/routes.js')(app);
 
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'index.html'));
 });
-app.listen(port);
+if(!module.parent){ app.listen(port); }
 console.log('server started');
