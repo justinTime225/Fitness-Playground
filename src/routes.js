be81775b09936d6ca4165d74916840068ce2189a
Justin <justinminh225@gmail.com>
2016-08-29 01:38:28 -0700

    Create routing for custom plan page

--- src/routes.js
+++ src/routes.js
@@ -3,9 +3,11 @@ import { Route, IndexRoute } from 'react-router';
 import App from './containers/App.js';
 import Table from './containers/Table.js';
 import Form from './containers/Form.js';
+import CustomPlan from './containers/customPlan.js';
 export default (
   <Route path="/" component={App}>
     <IndexRoute component={Form}/>
     <Route path="table" component={Table}/>
+    <Route path="custom" component={CustomPlan}/>
   </Route>
 );
